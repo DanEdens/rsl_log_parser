cf6c4fa0a643f473d5fa80859e4f2d70603a7c30
John Edens <dan.edens@geo-instruments.com>
2021-07-07 10:48:54 -0500

    adds mqtt logging if env set

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
 import sys
+import subprocess
+import os
+import paho.mqtt.client as paho
 
 #x = "Full normalization mode set [Time: 6/25/2021 5:41:33 PM, Resources: [Energy: 24.9111111111112], [Silver: 263430.073149072], [Token: 3.22583333333334], [Gem: 42], [Arena3X3ShopCurrency:       1278], [Arena3x3Token: 5], [Orb_MagicLow: 20], [Orb_MagicMid: 9], [Orb_MagicHigh: 4], [Orb_ForceMid: 97], [Orb_SpiritLow: 692], [Orb_SpiritMid: 111], [Orb_SpiritHigh: 28], [Orb_VoidLow: 45], [Orb_VoidMid: 80], [Orb_VoidHigh: 14], [Orb_ArcaneLow: 50], [Orb_ArcaneMid: 5], [Orb_ArcaneHigh: 26], [Medal_Bronze: 1], [Medal_Silver: 1], [Medal_Gold: 745], [AllianceBossKey:       1.04421296296297], [FractionWarKey_BannerLords: 6], [FractionWarKey_OgrynTribes: 10], [Forge_Magisteel: 3001], [Forge_Corehammer: 3], [Forge_SoulstoneRare: 367], [Forge_SoulstoneEpic:       434], [Forge_SoulstoneLeg: 20], [Forge_BloodstoneRare: 261], [Forge_BloodstoneEpic: 224], [Forge_BloodstoneLeg: 2], [Forge_NetherSpiderEggsRare: 200], [Forge_NetherSpiderEggsEpic: 19]      , [Forge_ScarabClawsRare: 7], [Forge_ScarabClawsEpic: 4], [Forge_MagmaDragonCoresRare: 406], [Forge_MagmaDragonCoresEpic: 129], [Forge_FrostSpiderSpinesRare: 7], [Forge_DragonBoneRare:       577], [Forge_DragonBoneEpic: 214], [Forge_GriffinFeatherRare: 211], [Forge_GriffinFeatherEpic: 1], [DoomTowerGoldKey: 10], [DoomTowerSilverKey: 10]]"
+
+try:
+    client = paho.Client('rsl_parser', clean_session=True)
+    client.connect(os.environ.get("AWSIP"), os.environ.get("AWSPORT"))
+except: pass
 
 def main(x):
     # print(x)
@@ -16,8 +24,12 @@ def main(x):
             pass
         myvars[name[2:]] = var[:-1]
 
-    energy = myvars['Energy'][:5]
-    print('Energy: ' + energy)
+    try:
+        client.publish('status/raid/energy', myvars['Energy'][:5], retain=True)
+        client.publish('status/raid/silver', myvars['Silver'], retain=True)
+        client.publish('status/raid/gems', myvars['Gem'], retain=True)
+    except: pass
+    print('Energy: ' + myvars['Energy'][:5])
     print('Silver: ' + myvars['Silver'])
     print('Gems: ' + myvars['Gem'])
 
