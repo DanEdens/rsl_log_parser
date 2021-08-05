4a70d03c6561e5247e6c4f607c818ffebc8ed1f7
Dan Edens <Dan.Edens@Geo-instruments.com>
2021-08-04 23:02:48 -0500

    Updates version

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 @echo off
-set log="%userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\242\log.txt"
+set log="%userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\245\log.txt"
 Echo Tailing Raid Log file..
 tail -f %log% | grep --line-buffered normalization | xargs -I {} python main.py {}
