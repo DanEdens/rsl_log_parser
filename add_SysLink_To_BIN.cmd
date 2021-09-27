c1faadfd1488df25c73630b1cdb5ee854be6f8ac
John Edens <dan.edens@geo-instruments.com>
2021-09-26 19:38:30 -0500

    add syslink generator and add version globbing for log file

---
+++ add_SysLink_To_BIN.cmd
@@ -0,0 +1,1 @@
+mklink "%bin%/raidlogparser.cmd" run.cmd
--- run.cmd
+++ run.cmd
@@ -1,4 +1,9 @@
 @echo off
-set log="%userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\245\log.txt"
+@REM set log="%userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\245\log.txt"
+pushd %userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\
+cd 2*
 Echo Tailing Raid Log file..
-tail -f %log% | grep --line-buffered normalization | xargs -I {} python main.py {}
+@REM instead of the file itself, it's easy to glob walk into the current version's folder..
+@REM tail -f %log% | grep --line-buffered normalization | xargs -I {} python main.py {}
+tail -f log.txt | grep --line-buffered normalization | xargs -I {} python main.py {}
+popd
