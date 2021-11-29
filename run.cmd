74cc9df5392adb2a7a8f03f038309b7e17e94402
John Edens <dan.edens@geo-instruments.com>
2021-11-28 20:28:30 -0600

    removes legacy method

--- run.cmd
+++ run.cmd
@@ -1,11 +1,11 @@
+@echo off
 :: Raid Shadow Legends PC client Log parser.
 :: Created by Dan Edens
 :: Requirments:
 :: xargs, grep, tail are unix commands included with Git Bash on Windows
 :: Python librays:
-:: logging - paho.mqtt
-
-@echo off
+:: 1. logging  
+:: 2. paho.mqtt  
 
 :main
 set repodir="%cd%"
@@ -16,16 +16,8 @@ tail -f log.txt | grep --line-buffered normalization | xargs -I {} python %repod
 popd
 goto:eof
 
-
 :errmsg
 echo Location not found..
 echo Please download from "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjpqs6igZ7zAhVLa28EHUhUBM4YABABGgJqZg&ae=2&ohost=www.google.com&cid=CAESQOD2pUITy8B_h97U08UnJljXhYgczPBbH_Ye98vku4frJfwXunTcikCNmgRKHmWh2gUAXB6xfJSsroiEGV-LUE4&sig=AOD64_0dmS4L7dWkOWop7soNwGDAAB7eCQ&q=&nis=1&ved=2ahUKEwiMzcGigZ7zAhX_mmoFHQ-2CkkQqyQoAHoECAIQEw&adurl=" 
-goto:eof
-
-:hardcoded
-:: (Legacy) Instead of the file itself, it's easier to glob walk into the current version's folder,
-:: from before i realized they made a whole folder per version instead of just using git.. 
-set log="%userprofile%\AppData\Local\Plarium\PlariumPlay\StandAloneApps\raid\250\log.txt
-tail -f %log% | grep --line-buffered normalization | xargs -I {} python __main__.py {}
 goto:eof
 
